be7d717280a3577ea92e7cf48fca4baabac2100f
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-10-08 11:57:02 -0500

    intial push

--- js/es6.js
+++ js/es6.js
@@ -1,99 +1,99 @@
-console.log("es6");
-// const - Will not change cant redifine 
-const a=8;
-console.log("a",a);
-// let - only used in block its defined in and IS reassignable --- Local
-let b=8;
-console.log("b",b );
-b =12
-console.log("b2",b)
-function varTest(){
-	var x=1;
-	if (true) {
-		var x=2;
-		console.log("inside if",x);
-	}
-	console.log("outside if", x)
-}
-varTest();
+// console.log("es6");
+// // const - Will not change cant redifine 
+// const a=8;
+// console.log("a",a);
+// // let - only used in block its defined in and IS reassignable --- Local
+// let b=8;
+// console.log("b",b );
+// b =12
+// console.log("b2",b)
+// function varTest(){
+// 	var x=1;
+// 	if (true) {
+// 		var x=2;
+// 		console.log("inside if",x);
+// 	}
+// 	console.log("outside if", x)
+// }
+// varTest();
 
-function letTest(){
-	let y=1;
-	if (true) {
-		let y=12;
-		console.log("inside if",y);
-	}
-		console.log("outside if", y)
-}
-letTest();
-// arrow functions
-var sum = function(){
-	let a=1, b=2;
-	return a+b;
-}
-console.log("hardcodded sum in ES5",sum());
+// function letTest(){
+// 	let y=1;
+// 	if (true) {
+// 		let y=12;
+// 		console.log("inside if",y);
+// 	}
+// 		console.log("outside if", y)
+// }
+// letTest();
+// // arrow functions
+// var sum = function(){
+// 	let a=1, b=2;
+// 	return a+b;
+// }
+// console.log("hardcodded sum in ES5",sum());
 
-var sum2 = ()=> {
-	let a=1, b=2;
-	return a+b;
-}
-console.log("hardcodded sum in ES6",sum2());
+// var sum2 = ()=> {
+// 	let a=1, b=2;
+// 	return a+b;
+// }
+// console.log("hardcodded sum in ES6",sum2());
 
-var reflect = function(value){
-	return value;
-}
-console.log("refect in ES5",reflect("ES5 is so yesterday"));
+// var reflect = function(value){
+// 	return value;
+// }
+// console.log("refect in ES5",reflect("ES5 is so yesterday"));
 
-var reflect2=(value)=> {
-	return value;
-}
-console.log("refect in ES6",reflect2("ES6 is the new hotness"));
+// var reflect2=(value)=> {
+// 	return value;
+// }
+// console.log("refect in ES6",reflect2("ES6 is the new hotness"));
 
-var realSum = function(num1, num2) {
-	return num1+num2;
-}
-	console.log("realsum ES5",realSum(2,3));
+// var realSum = function(num1, num2) {
+// 	return num1+num2;
+// }
+// 	console.log("realsum ES5",realSum(2,3));
 
-var realsum2 = (num1,num2) => {
-	return num1 + num2;
-}
-console.log("realsum ES6",realsum2(5,4));
+// var realsum2 = (num1,num2) => {
+// 	return num1 + num2;
+// }
+// console.log("realsum ES6",realsum2(5,4));
 
-// string templates - 
-let m = "biteme";
-console.log("m",m );
-let r = `hello my name is ${m}`;
-console.log("r",r );
+// // string templates - 
+// let m = "biteme";
+// console.log("m",m );
+// let r = `hello my name is ${m}`;
+// console.log("r",r );
 
-// property shorthand
+// // property shorthand
 
 
-// object lilteral property shorthand
-let wow="Hi there", es6="ES6", numNum = ()=>{console.log("howdy")}
+// // object lilteral property shorthand
+// let wow="Hi there", es6="ES6", numNum = ()=>{console.log("howdy")}
 
-let myOldObject = {
-	wow:wow,
-	es6:es6,
-	numNum:numNum
-}
-console.log("myOldObject",myOldObject );
-// can do if key and value are the same
-let myNewObject = {
-	wow,es6,numNum
-}
-console.log("myNewObject",myNewObject );
-// method properties
-var e5Obj = {
-	foo:function(){console.log("foo")},
-	bar:function(){console.log("foo")}
-}
-console.log("e5Obj",e5Obj);
+// let myOldObject = {
+// 	wow:wow,
+// 	es6:es6,
+// 	numNum:numNum
+// }
+// console.log("myOldObject",myOldObject );
+// // can do if key and value are the same
+// let myNewObject = {
+// 	wow,es6,numNum
+// }
+// console.log("myNewObject",myNewObject );
+// // method properties
+// var e5Obj = {
+// 	foo:function(){console.log("foo")},
+// 	bar:function(){console.log("foo")}
+// }
+// console.log("e5Obj",e5Obj);
 
-var es6Obj = {
-	foo(){console.log("foo")},
-	bar(){console.log("bar")}
-}
-console.log("es6Obj",es6Obj)
+// var es6Obj = {
+// 	foo(){console.log("foo")},
+// 	bar(){console.log("bar")}
+// }
+// console.log("es6Obj",es6Obj)
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("main");
+/*global $*/
+// use the lineup if "$" is not in Gruntfile predef part
+
+
+"use strict";
+const outputDiv = $("#output");
+console.log("main", outputDiv);
